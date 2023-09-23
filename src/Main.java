83bb2b4e85e20b49e75d815f88ec533d6a711d8d
Andrew Kurylyk <andronniy7@gmail.com>
2023-09-23 17:08:55 +0300

    Main object three

--- src/Main.java
+++ src/Main.java
@@ -235,16 +235,29 @@
 //        System.out.println(myTime);
 //    }
 //}
+//
+//import java.time.LocalDateTime;
+//import java.time.format.DateTimeFormatter;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        LocalDateTime myDateObj = LocalDateTime.now();
+//        System.out.println("Before formatting: " + myDateObj);
+//        DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss");
+//        String formattedDate = myDateObj.format(myFormatObj);
+//        System.out.println("After formatting: " + formattedDate);
+//    }
+//}
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        LocalDateTime myDateObj = LocalDateTime.now();
-        System.out.println("Before formatting: " + myDateObj);
-        DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss");
-        String formattedDate = myDateObj.format(myFormatObj);
-        System.out.println("After formatting: " + formattedDate);
+        ArrayList<String> cars = new ArrayList<String>();
+        cars.add("Volvo");
+        cars.add("BMW");
+        cars.add("Ford");
+        cars.add("Mazda");
+        System.out.println(cars);
     }
 }
